4e0b8a9c616eb420efd330c224b78c2725cda2aa
Kuldeep Singh <pappuks@gmail.com>
2024-02-04 22:14:40 -0800

    First commit

---
+++ app.py
@@ -0,0 +1,88 @@
+from langchain.chains import RetrievalQA
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.vectorstores import Chroma
+from langchain.llms import Ollama
+from langchain import PromptTemplate
+import chromadb
+import os
+import argparse
+import time
+import chainlit as cl
+
+model = os.environ.get("MODEL", "llama2:13b")
+# For embeddings model, the example uses a sentence-transformers model
+# https://www.sbert.net/docs/pretrained_models.html 
+# "The all-mpnet-base-v2 model provides the best quality, while all-MiniLM-L6-v2 is 5 times faster and still offers good quality."
+embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME", "all-MiniLM-L6-v2")
+persist_directory = os.environ.get("PERSIST_DIRECTORY", "db")
+target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
+
+custom_prompt_template = """Use the following pieces of information to answer the user's question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer below and nothing else.
+Helpful and Caring answer:
+"""
+
+def custom_prompt():
+    """
+    Prompt template for QA retrieval for each vectorstore
+    """
+    prompt = PromptTemplate(template=custom_prompt_template,
+                            input_variables=['context', 'question'])
+    return prompt
+
+
+@cl.on_chat_start
+async def start():
+    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+
+    db = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
+
+    retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
+    # activate/deactivate the streaming StdOut callback for LLMs
+    callbacks = [StreamingStdOutCallbackHandler()]
+
+    llm = Ollama(model=model, callbacks=callbacks)
+
+    qa = RetrievalQA.from_chain_type(
+        llm=llm, 
+        chain_type="stuff", 
+        retriever=retriever, 
+        return_source_documents= True,
+        chain_type_kwargs={'prompt': custom_prompt()})
+    chain = qa
+    msg = cl.Message(content="Starting the bot...")
+    await msg.send()
+    msg.content = "Hi, Welcome to Medical Bot. What is your query?"
+    await msg.update()
+
+    cl.user_session.set("chain", chain)
+
+@cl.on_message
+async def main(message):
+    chain = cl.user_session.get("chain") 
+    cb = cl.AsyncLangchainCallbackHandler(
+        stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
+    )
+    cb.answer_reached = True
+    #res = await chain.acall(message, callbacks=[cb])
+    res = chain(message)
+    answer = res["result"]
+    sources = res["source_documents"]
+
+    if sources:
+        answer += f"\nSources:" + str(sources)
+    else:
+        answer += "\nNo sources found"
+
+    await cl.Message(content=answer).send()
+
+
+
+
+
